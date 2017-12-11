2d05870a9d320bea9c7b8814070d3b2bddc3bf36
Julia Schatzmann <julia.schatzmann@students.fhv.at>
2017-12-11 09:59:42 +0100

    added vollbrecht file

---
+++ src/RobotWithCameraAccelerometerWheelEncoder.java
@@ -0,0 +1,63 @@
+
+import com.cyberbotics.webots.controller.Camera;
+import com.cyberbotics.webots.controller.DifferentialWheels;
+
+
+public abstract class RobotWithCameraAccelerometerWheelEncoder extends DifferentialWheels {
+	
+	public WebotsRobot(int sensorResponse) {
+
+//		Camera.pixelGetRed(0);
+		
+		this.getCamera(getCameraName()).enable(sensorResponse);
+		
+		this.getAccelerometer(getAccelerometerName()).enable(sensorResponse);
+		
+		this.enableEncoders(sensorResponse);
+	}
+	
+	
+	protected String getCameraName() {
+		return CAMERA;
+	}
+
+	
+	protected String getAccelerometerName() {
+		return ACCELEROMETER;
+	}
+
+	
+	public int getCameraWidth() {
+		return this.getCamera(getCameraName()).getWidth();
+	}
+	
+	
+	public int getCameraHeight() {
+		return this.getCamera(getCameraName()).getHeight();
+	}
+
+
+	
+	public int[] getCameraValues() {
+//		Camera.pixelGetBlue(1);
+		return this.getCamera(getCameraName()).getImage();
+	}
+	
+	
+	public double getXAccel() {
+		return this.getAccelerometer(getAccelerometerName()).getValues()[0];
+	}
+
+	
+	public double getYAccel() {
+		return this.getAccelerometer(getAccelerometerName()).getValues()[1];
+	}
+	
+	
+	public double getZAccel() {
+		return this.getAccelerometer(getAccelerometerName()).getValues()[2];
+	}
+	
+	public double[] getEncoderValues()  {
+		return new double[] { this.getLeftEncoder(), this.getRightEncoder()};
+}
